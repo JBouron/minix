17e414d046456bfaafbf628a9bff18aeda081c28
JBouron <justinien.bouron@sydney.edu.au>
2019-02-20 15:41:53 +1100

    Allow kernel calls to any proc

--- minix/kernel/system.c
+++ minix/kernel/system.c
@@ -108,11 +108,11 @@ static int kernel_call_dispatch(struct proc * caller, message *msg)
 	  printf("SYSTEM: illegal request %d from %d.\n",
 			  call_nr,msg->m_source);
 	  result = EBADREQUEST;			/* illegal message type */
-  }
+  /*}
   else if (!GET_BIT(priv(caller)->s_k_call_mask, call_nr)) {
 	  printf("SYSTEM: denied request %d from %d.\n",
 			  call_nr,msg->m_source);
-	  result = ECALLDENIED;			/* illegal message type */
+	  result = ECALLDENIED;	*/		/* illegal message type */
   } else {
 	  /* handle the system call */
 	  if (call_vec[call_nr])
